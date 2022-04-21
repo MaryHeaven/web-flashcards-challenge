fb6b59ea81a4b4e7e35ba2c6e904b2b5658b7903
Mariya_Miroshnichenko <mar58799263@yandex.ru>
2022-04-21 18:51:28 +0300

    process...

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const express = require('express');
 
 // routers
-// const mainRouters = require('./routes/main.route');
+const mainRouters = require('./routes/main.route');
 // const adminRouter = require('./routes/admin.route')
 
 // app && PORT
@@ -15,7 +15,7 @@ config(app);
 
 // routes
 // app.use('/', mainRouters);
-// app.use('/admin', adminRouter);
+app.use('/', mainRouters);
 
 // listen
 app.listen(PORT, () => { console.log(`******* Server started at port: ${PORT} *******`); });
---
+++ public/js/client.js
@@ -0,0 +1,6 @@
+const start = document.querySelector('#start');
+
+start.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+});
